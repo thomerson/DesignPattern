049d6be7b8924eabfff6c6fa808b075ffdc3ff0a
thomerson <756155037@qq.com>
2022-11-14 22:27:15 +0800

    factory pattern

---
+++ Thomerson.DesignPattern/工厂方法模式/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 工厂方法模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factory = new CarFactory();
+            Product car = factory.Create();
+
+            Factory phoneFactory = new PhoneFactory();
+            Product phone = phoneFactory.Create();
+        }
+    }
+}
