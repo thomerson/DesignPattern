d2c90755c8ca43609cb636fbab52250a174813cd
thomerson <756155037@qq.com>
2022-11-15 22:44:48 +0800

    prototype pattern

---
+++ Thomerson.DesignPattern/原型模式/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 原型模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MonkeyKing monkeyKing = new MonkeyKing("齐天大圣");
+
+            MonkeyKingPrototype prototype1 = monkeyKing.Clone();
+
+            MonkeyKingPrototype prototype2 = monkeyKing.Clone();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
