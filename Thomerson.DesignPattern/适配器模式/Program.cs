9d0d8e29a6a93ba62e8ca84b5bbd1f5138db051d
thomerson <756155037@qq.com>
2022-11-16 21:53:26 +0800

    adapter pattern

---
+++ Thomerson.DesignPattern/适配器模式/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 适配器模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var powerAdapter = new PowerAdapter();
+            powerAdapter.TwoPower();
+
+            powerAdapter.ThreePower();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
