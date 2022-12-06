e474db680ad128b6622d42ac2d95d48e4a258106
thomerson <756155037@qq.com>
2022-12-06 21:53:27 +0800

    代理模式

--- Thomerson.DesignPattern/代理模式/Program.cs
+++ Thomerson.DesignPattern/代理模式/Program.cs
@@ -9,9 +9,9 @@ namespace 代理模式
     /// 1、和适配器模式的区别：适配器模式主要改变所考虑对象的接口，而代理模式不能改变所代理类的接口。 
     /// 2、和装饰器模式的区别：装饰器模式为了增强功能，而代理模式是为了加以控制。
     /// </summary>
-    class Client
+    class Program
     {
-        void Test()
+        static void Main(string[] args)
         {
             var station = new ProxyStation("上海站");
 
